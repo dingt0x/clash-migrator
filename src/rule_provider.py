082956deaffa9e77ca7e23e8182fe9f561cfe15c
dingt0x <dingt0x@gmail.com>
2025-12-28 14:33:58 +0800

    update rule-providers

--- src/rule_provider.py
+++ src/rule_provider.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import aiofiles
+import hashlib
+
 class RuleProviderRender:
     def __init__(self, tpl_file: Path, fqdn:str):
         self.tpl_file = tpl_file
@@ -12,6 +14,8 @@ class RuleProviderRender:
         if not self.tpl_file.is_file():
             raise FileNotFoundError(f"Template file not found: {self.tpl_file}")
 
+
         async with aiofiles.open(str(self.tpl_file), mode='r', encoding='utf-8') as f:
+            ruleset_path=hashlib.md5(self.fqdn.encode()).hexdigest()[0:8]
             content = await f.read()
-            return content.format(FQDN_RULE_PROVIDER=self.fqdn)
+            return content.format(FQDN_RULE_PROVIDER=self.fqdn,ruleset_path=ruleset_path)
